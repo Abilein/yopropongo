0e6191c4331e6671e7fe70c34dd45ffc8773c421
Grover Callisaya <grover.callisaya.dg@gmail.com>
2017-05-02 12:03:45 -0400

    Base of Porject completed

--- app/src/main/java/com/xiberty/propongo/Constants.java
+++ app/src/main/java/com/xiberty/propongo/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String CREDENTIAL_COLLECTION = "credential_collection";
     public static final String USER_PROFILE = APPLICATION_PREFIX  + "_user";
 
-    public static final String SERVER_URL = "http://192.168.0.102:8000";
+    public static final String SERVER_URL = "http://192.168.0.102:9000";
     public static final String CLIENT_ID = "bgzxrqgTnmCzHxq5CJTusph5g7WpvhQmHLGQQehH";
     public static final String CLIENT_SECRET = "L7B0YRVoIy62a8PYCZc2r3VGPQscujFW6nwV97WV7UpVlMNVHEsV5PIyCs7bWnEGZV3XKZhoTYjq6fysMWRNhTq8aSBCjFBjwW6v57ccXViWmu0DeTC7111aNlHa0XPU";
 
@@ -30,17 +30,17 @@ public class Constants {
      **/
 
     // Auth
-    public static final String LOGIN_ENDPOINT = "/api/auth/login/";
-    public static final String LOGOUT_ENDPOINT = "/api/auth/logout/";
-    public static final String RESET_PASSWORD_ENDPOINT = "/api/auth/reset-password/";
-    public static final String REGISTER_ENDPOINT = "/api/auth/register/";
-    public static final String FACEBOOK_LOGIN_ENDPOINT = "/api/auth/facebook/login/";
+    public static final String LOGIN_ENDPOINT = "/api/credentials/login/";
+    public static final String LOGOUT_ENDPOINT = "/api/credentials/revoke-token/";
+    public static final String RESET_PASSWORD_ENDPOINT = "/api/credentials/reset-password/";
+    public static final String REGISTER_ENDPOINT = "/api/credentials/register/";
+    public static final String FACEBOOK_LOGIN_ENDPOINT = "/api/credentials/facebook/login/";
 
 
     // Account
-    public static final String PROFILE_ENDPOINT = "/api/account/profile/";
-    public static final String CHANGE_EMAIL_ENDPOINT = "/api/account/change-email/";
-    public static final String CHANGE_PASSWORD_ENDPOINT = "/api/account/change-password/";
+    public static final String PROFILE_ENDPOINT = "/api/accounts/profile/";
+    public static final String CHANGE_EMAIL_ENDPOINT = "/api/accounts/change-email/";
+    public static final String CHANGE_PASSWORD_ENDPOINT = "/api/accounts/change-password/";
 
 
 }
